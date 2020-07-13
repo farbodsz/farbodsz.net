58a68c8852c6d3e99c505c3097540abb9f1cd774
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-07-13 21:31:12 +0100

    Refine subtitle text and add link to warwick.ac.uk

--- src/components/sections/Intro.js
+++ src/components/sections/Intro.js
@@ -9,7 +9,11 @@ export default function Intro() {
         <p className={intro.desc}>Hi, my name is</p>
         <h1 className={intro.name}>Farbod Salamat-Zadeh</h1>
         <p className={intro.desc}>
-          I'm a 2nd year CompSci student at the <b>University of Warwick.</b>
+          I'm a second year CS student at the{" "}
+          <a href={"https://warwick.ac.uk/"} target="_blank">
+            <b>University of Warwick</b>
+          </a>
+          .
         </p>
       </Layout>
     </div>
