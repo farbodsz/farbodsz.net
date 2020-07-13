c968180453b9cef80370459a95282e61745a2a00
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-07-13 21:03:23 +0100

    Rename CSS classes to adhere to naming convention

--- src/components/sections/Intro.js
+++ src/components/sections/Intro.js
@@ -4,11 +4,11 @@ import Layout from "../Layout";
 
 export default function Intro() {
   return (
-    <div className={intro.Container}>
+    <div className={intro.container}>
       <Layout>
-        <p className={intro.Body}>Hi, my name is</p>
-        <h1 className={intro.Heading}>Farbod Salamat-Zadeh</h1>
-        <p className={intro.Body}>
+        <p className={intro.desc}>Hi, my name is</p>
+        <h1 className={intro.name}>Farbod Salamat-Zadeh</h1>
+        <p className={intro.desc}>
           I'm a 2nd year CompSci student at the <b>University of Warwick.</b>
         </p>
       </Layout>
