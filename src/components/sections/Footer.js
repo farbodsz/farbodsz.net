e04b7b46c333bb97ec367f44743c7d5b6f1db6df
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-07-13 21:26:11 +0100

    Improve styling and text of footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -6,8 +6,10 @@ export default function Footer() {
   return (
     <footer className={styles.container}>
       <Layout>
-        <p>Copyright (C) 2020 Farbod Salamat-Zadeh</p>
-        <p>
+        <p className={styles.mainText}>
+          Designed and developed by Farbod Salamat-Zadeh, 2020
+        </p>
+        <p className={styles.attribution}>
           Skills icons made by{" "}
           <a href={"https://freeicons.io/profile/3"}>icon king</a> from{" "}
           <a href={"https://www.freeicons.io/"}>www.freeicons.io</a>
