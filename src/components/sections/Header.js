f6d765b4bf49a3439567104671ce6c45d0e861a1
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-07-22 11:10:32 +0100

    Add Stack Overflow icon

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Header.module.sass";
 import iconGithub from "../../icons/github.svg";
+import iconSO from "../../icons/so.svg";
 import iconLinkedIn from "../../icons/linkedin.svg";
 
 export default function Header() {
@@ -8,6 +9,9 @@ export default function Header() {
     <header className={styles.container}>
       <a href="https://github.com/farbodsz/" target="_blank">
         <img className={styles.icon} src={iconGithub} alt={"GitHub"} />
+      </a>
+      <a href="https://stackoverflow.com/users/4230345/" target="_blank">
+        <img className={styles.icon} src={iconSO} alt={"Stack Overflow"} />
       </a>
       <a href="https://www.linkedin.com/in/farbodsz/" target="_blank">
         <img className={styles.icon} src={iconLinkedIn} alt={"LinkedIn"} />
