ee0773d36e454992ba685ddc8ca3d0f8195b92f5
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-12-13 15:22:13 +0000

    Add ProjectItem component

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,109 @@
+import React from "react";
+import styles from "./ProjectItem.module.scss";
+
+/**
+ * Separator to use between years.
+ *
+ * This is the en-dash character.
+ */
+const SEP_YEARS = "–";
+
+/**
+ * Returns a string representing the year range.
+ *
+ * @param {number?} startYear
+ * @param {number?} endYear
+ *
+ * If start year is given but no end year, then it will be assumed it is until
+ * the present.
+ * If end year is given but no start year, then it will be assumed they are in
+ * the same year.
+ */
+function formatYears(startYear, endYear) {
+  if (startYear) {
+    var endYearText = endYear
+      ? `'${endYear.toString().substring(2)}`
+      : "present";
+    return `(${startYear}${SEP_YEARS}${endYearText})`;
+  } else if (endYear) {
+    return `(${endYear})`;
+  } else {
+    return "";
+  }
+}
+
+/**
+ * Returns a string representing the list of programming languages.
+ *
+ * @param {array} langs
+ */
+function formatLangs(langs) {
+  return langs.join(" | ");
+}
+
+/**
+ * Props:
+ *  - title
+ *  - startYear
+ *  - endYear
+ *  - langs (array)
+ *  - img
+ *  - github
+ *  - link
+ *  - children (the body)
+ */
+export default function ProjectItem(props) {
+  const hasYears = props.startYear || props.endYear;
+
+  const subtitleText = props.langs ? formatLangs(props.langs) : null;
+  var subtitleElement = "";
+
+  if (subtitleText || hasYears) {
+    const yearsText = formatYears(props.startYear, props.endYear);
+    subtitleElement = (
+      <h4 className={styles.subtitle}>
+        {subtitleText} <span className={styles.years}>{yearsText}</span>
+      </h4>
+    );
+  }
+
+  const githubButton = props.github ? (
+    <div className={styles.button}>
+      <a href={props.github} target="_blank" rel="noreferrer">
+        View GitHub repo
+      </a>
+    </div>
+  ) : (
+    ""
+  );
+
+  const linkButton = props.link ? (
+    <div className={styles.button}>
+      <a href={props.link} target="_blank" rel="noreferrer">
+        View project
+      </a>
+    </div>
+  ) : (
+    ""
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h3>{props.title}</h3>
+        {subtitleElement}
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.imgContainer}>
+          <img className={styles.img} src={props.imgSrc} />
+        </div>
+        <div className={styles.description}>
+          {props.children}
+          <div className={styles.buttonContainer}>
+            {linkButton} {githubButton}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-import favicon from "../icons/favicons/favicon.ico";
-import iconApple from "../icons/favicons/apple-touch-icon.png";
-import icon16 from "../icons/favicons/favicon-16x16.png";
-import icon32 from "../icons/favicons/favicon-32x32.png";
 import Frame from "../components/Frame";
 import Header from "../components/Header";
 import Intro from "../components/Intro";
 import Footer from "../components/Footer";
+import Section from "../components/Section";
+import ProjectItem from "../components/ProjectItem";
+
+import favicon from "../icons/favicons/favicon.ico";
+import iconApple from "../icons/favicons/apple-touch-icon.png";
+import icon16 from "../icons/favicons/favicon-16x16.png";
+import icon32 from "../icons/favicons/favicon-32x32.png";
+
+import pokedataImg from "../images/pokedata.png";
 
 export default function Home() {
   const NAME = "Farbod Salamat-Zadeh";
@@ -34,7 +39,28 @@ export default function Home() {
       <Frame>
         <Header />
         <Intro />
+
+        <Section title="Projects">
+          <ProjectItem
+            title="Pokédex App"
+            startYear={2016}
+            endYear={2017}
+            langs={["Java", "Kotlin", "Android", "SQLite"]}
+            imgSrc={pokedataImg}
+            github="https://github.com/farbodsz/PokedexApp"
+          >
+            <p>
+              Pokédex is a database that Pokémon-enthusiasts can browse, filter,
+              and search through to find descriptions and stats of Pokémon.
+            </p>
+            <p>
+              The app uses a colourful, intuitive design and includes features
+              like being able to compare Pokémon side-by-side.
+            </p>
+          </ProjectItem>
+        </Section>
       </Frame>
+
       <Footer />
     </div>
   );
