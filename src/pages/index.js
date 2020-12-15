ce95fe964df3cb97481b264cf929b24c93018e4f
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2020-12-15 13:47:29 +0000

    Remove react-reveal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Fade } from "react-reveal";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -39,15 +38,9 @@ export default function Home() {
       <Header />
 
       <Layout>
-        <Fade>
-          <Skills />
-        </Fade>
-        <Fade>
-          <FeaturedProjects />
-        </Fade>
-        <Fade>
-          <OtherProjects />
-        </Fade>
+        <Skills />
+        <FeaturedProjects />
+        <OtherProjects />
       </Layout>
 
       <Footer />
