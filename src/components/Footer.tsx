3dc429ded8688b654af7addf21ca8c48bd8be4fc
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-06-15 19:46:57 +0100

    Update screenshot and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,8 @@ export default function Footer(): JSX.Element {
           Designed and developed by Farbod Salamat-Zadeh.
         </p>
         <p className={styles.text}>
-          Built using React.{" "}
+          Built using React and Gatsby. Hosted with Netlify.
+          <br />
           <a href="https://github.com/farbodsz/farbodsz.net">
             View source code on GitHub.
           </a>
